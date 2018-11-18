a15ae600eda91afad619c02002080d7285811e50
Sigma902 <cdoran@mac.com>
2018-11-17 20:59:05 -0600

    Delete fix_file_perms.sh

--- OOBE-Files/setupassistant/userland/fix_file_perms.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-/bin/echo "Status: Checking file permissions..." > /var/tmp/depnotify.log
-/usr/bin/chflags -R nouchg ~
-/usr/sbin/diskutil resetUserPermissions / `id -u`
-/bin/echo "Status: File permissions are correct." > /var/tmp/depnotify.log
-
-exit 0
