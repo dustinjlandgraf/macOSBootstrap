bc7f154f83d5ae11afc6687c1ab518e7fa8d375b
Sigma902 <cdoran@mac.com>
2018-11-29 23:50:05 -0600

    Removed DEPNotify logging
    Now intended as preflight rather than wait for user land.

--- OOBE-Files/userland/setTimeServer.sh
+++ OOBE-Files/userland/setTimeServer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-/bin/echo "Status: Setting time server" > /var/tmp/depnotify.log
 /usr/sbin/systemsetup -setusingnetworktime OFF
 /usr/sbin/systemsetup -setnetworktimeserver time.nist.gov
 /bin/sleep 1
@@ -8,4 +7,4 @@
 /bin/sleep 10
 /usr/bin/defaults write /Library/Preferences/com.apple.mdmclient AutoRenewCertificatesEnabled -bool YES
 
-exit 0
+exit 1
