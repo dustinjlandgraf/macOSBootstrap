b02a0220c61510c9fd715307cb0d4119b1bb36d8
Sigma902 <cdoran@mac.com>
2018-11-29 21:43:21 -0600

    Removed stout suppressions

--- OOBE-Files/userland/setTimeServer.sh
+++ OOBE-Files/userland/setTimeServer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 /bin/echo "Status: Setting time server" > /var/tmp/depnotify.log
-/usr/sbin/systemsetup -setusingnetworktime OFF >/dev/null 2>&1
-/usr/sbin/systemsetup -setnetworktimeserver time.nist.gov >/dev/null 2>&1
+/usr/sbin/systemsetup -setusingnetworktime OFF
+/usr/sbin/systemsetup -setnetworktimeserver time.nist.gov
 /bin/sleep 1
-/usr/sbin/systemsetup -setusingnetworktime ON >/dev/null 2>&1
+/usr/sbin/systemsetup -setusingnetworktime ON
 /bin/sleep 10
-/usr/bin/defaults write /Library/Preferences/com.apple.mdmclient AutoRenewCertificatesEnabled -bool YES >/dev/null 2>&1
+/usr/bin/defaults write /Library/Preferences/com.apple.mdmclient AutoRenewCertificatesEnabled -bool YES
 
 exit 0
